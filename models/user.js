2dd3b7c09aa5b29169713c258b3a36734f5704e1
forente <zerofox16@gmail.com>
2016-07-27 11:56:21 -0500

    just before adding login

---
+++ models/user.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+  username:{ type:String, required:true, index: {unique:true}},
+  password:{ type:String, required:true}
+});
+
+UserSchema.pre('save',function(next){
+  var user = this;
+
+  // hashes  new passwords only
+  if (!user.isModified('password')){
+    return next();
+  }
+
+  // generates the salt
+   bcrypt.genSalt(SALT_WORK_FACTOR, function(err,salt){
+     if(err){
+       return next(err);
+     }
+
+    bcrypt.hash(user.password, salt, function(err,hash){
+      if(err){
+        return next(err);
+      }
+
+      // override cleartext
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb){
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if(err){
+      return cb(err);
+    }
+    cb(null,isMatch);
+  });
+};
+
+module.exports = mongoose.model('User',UserSchema);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var passport = require('passport');
 var mongoose = require('mongoose');
 
 var session = require('express-session');
+var User = require('./models/user');
 
 var app = express();
 
@@ -33,8 +34,35 @@ app.use(passport.session());
 passport.use('local', new localStrategy({ passReqToCallback: true, usernameField: 'username'},
   function(req, username, password, done ){
     //TODO :
-  }
-));
+    User.findOne({username: username}, function(err,user){
+      if(err){
+        throw err;
+      }
+
+      if (isMatch){
+        return done(null, user);
+      }
+      else {
+        doen(null, false, {message: 'Incorrect username and password.'});
+      }
+    });
+  })
+);
+
+passport.serializedUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.serializedUser(function(user, done){
+  User.findById(id, function(err, user){
+    if(err){
+      return done(err);
+    }
+    done(null, user);
+  });
+});
+
+
 
 var server = app.listen(3000, function(){
   var port = server.address().port;
